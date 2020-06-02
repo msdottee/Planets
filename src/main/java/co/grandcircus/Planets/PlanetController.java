326e5f8a8e53bbab044916ba54b897de13031599
ashley <ashleykubera@gmail.com>
2020-06-02 16:27:42 -0400

    code clean up

--- src/main/java/co/grandcircus/Planets/PlanetController.java
+++ src/main/java/co/grandcircus/Planets/PlanetController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import co.grandcircus.Planets.model.PlanetColors;
 import co.grandcircus.Planets.model.PlanetInfo;
 
 @Controller
